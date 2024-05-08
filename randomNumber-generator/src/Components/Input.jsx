eace12ae51e08d1442a9db6d9106b859da09bb66
kay-k181 <kaykaywill@live.co.uk>
2024-05-08 13:09:17 +0100

    inital commit

---
+++ randomNumber-generator/src/Components/Input.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Input({value, setValue, setPage}){
+    function handleSubmit(event){
+        event.preventDefault()
+        if (value < 1 || value > 20 || isNaN(value)){
+            alert('Please enter a value between 1 and 20')
+        } else {
+            setPage('Go to RandomGen')
+        }
+    }
+
+    return (
+        <div>
+            <div>How many numbers would you like to generate between 1 and 20?</div>
+            <form >
+                <label htmlFor="">
+                    Please enter here:
+                    <input 
+                        type="number" 
+                        value={value}
+                        onChange={(e) => setValue(parseInt(e.target.value))}
+                    />
+                </label>
+            </form>
+            <button onClick={handleSubmit}>Next Step</button>            
+        </div>
+        
+    )
+}
